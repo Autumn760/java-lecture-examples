1fafb204debf1c4bc5ec63cd7ea75ed138afc6fd
Stephanie Ontiveros <autumn760@gmail.com>
2023-09-18 12:17:12 -0500

    class02 ArrayListExamples complete

--- LectureExamples/src/main/java/class02/ArrayListExamples.java
+++ LectureExamples/src/main/java/class02/ArrayListExamples.java
@@ -1,5 +1,8 @@
 package class02;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ArrayListExamples {
 
     public static void main(String[] args) {
@@ -8,7 +11,9 @@ public class ArrayListExamples {
 
         // TODO: Declare two empty ArrayLists
         // elves, for data of type String
+        ArrayList<String> elves = new ArrayList<>();
         // ages, for data of type Integer
+        ArrayList<Integer> ages = new ArrayList<>();
 
 
         /* ADDING ELEMENTS TO ARRAYLISTS */
@@ -16,23 +21,35 @@ public class ArrayListExamples {
         // TODO: Add elements to each of arraylists above
 
         // 4 well-known elves
+        elves.add("Legolas");
+        elves.add("Arwen");
+        elves.add("Elrond");
+        elves.add("Galadriel");
 
         // 4 ages of elves above
+
+        ages.add(2931);
+        ages.add(2901);
+        ages.add(6497);
+        ages.add(8382);
 
 
         /* PRINTING ARRAYLISTS */
 
         // TODO: Print each arraylist
+        System.out.println(elves);
+        System.out.println(ages);
 
 
         /* USING ARRAYLIST & COLLECTIONS METHODS */
 
         // TODO: Print the size of elves
-
+        System.out.println(elves.size());
         // TODO: Check to see if ages contains 2901 and print the result
-
+        System.out.println(ages.contains(2901));
         // TODO: Sort elves and then print it
-
+        Collections.sort(elves);
+        System.out.println(elves);
 
         // And now the ages of the elves are not in the same order...
         // We will fix this with HashMapsa
