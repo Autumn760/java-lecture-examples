a6792c26df977a0ac2620187d150d24eaaf11f85
Stephanie Ontiveros <autumn760@gmail.com>
2023-09-18 11:18:34 -0500

    class01 complete

--- LectureExamples/src/main/java/class01/Main.java
+++ LectureExamples/src/main/java/class01/Main.java
@@ -37,29 +37,32 @@ public class Main {
         /* WRITING METHODS */
 
         // TODO: After coding your method below main(), call it here and print the result
-
+        System.out.println(getFullName("Matt", "Mercer"));
 
 
         /* USING METHODS FROM OTHER CLASSES */
 
         // TODO: Call each of the static methods from the Examples class
+        Examples.sayHello("Bob");
 
+        System.out.println(Examples.addThreeNums(2,5,9));
 
         /* USING SCANNER TO GET USER INPUT */
 
         // TODO: Declare and initialize a Scanner object using System.in
+        Scanner input = new Scanner(System.in);
 
         // TODO: Get user input
         // Precede your input with output to the user so they know what you need
-
+        System.out.println("Enter a whole number: ");
         // Receive the input and store it in a variable
-
+        int userNumber = input.nextInt();
         // Do something with the stored user input
-
+        System.out.println("Your number is " + userNumber);
         // Always close the Scanner object at the end of the main method
         // for efficiency and security
         // TODO: Close Scanner
-
+        input.close();
     }
 
     // TODO: Code a method to return a full name as a String using firstName and lastName
